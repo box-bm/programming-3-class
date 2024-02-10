0087440f76a807172a401fa9c6b61467b7730fb2
Brandon Manzo <brandong.manzog@icloud.com>
2024-02-10 13:20:38 -0600

    feat: added programming class excercise

---
+++ pilas/pilas/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace pilas
+{
+  internal class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Ingrese una cadena con parentesis, corchetes y llaves:");
+      string input = Console.ReadLine() ?? "";
+
+      if (VerificateBalance(input) && input.Count() > 0)
+      {
+        Console.WriteLine("Cadena aceptada");
+        return;
+      }
+
+      Console.WriteLine("Cadena rechazada");
+    }
+
+    static bool VerificateBalance(string input)
+    {
+      Stack<char> stack = new Stack<char>();
+
+      foreach (char c in input)
+      {
+        if (IsOpenChar(c))
+        {
+          stack.Push(c);
+        }
+        else if (IsClosedChar(c))
+        {
+          if (IsEqualChar(stack.Peek(), c) || !(stack.Count == 0))
+          {
+            stack.Pop();
+          }
+        }
+      }
+
+      return stack.Count == 0;
+    }
+
+    static bool IsOpenChar(char c)
+    {
+      return c == '(' || c == '[' || c == '{';
+    }
+
+    static bool IsClosedChar(char c)
+    {
+      return c == ')' || c == ']' || c == '}';
+    }
+
+    static bool IsEqualChar(char first, char second)
+    {
+      return (first == '(' && second == ')') || (first == '[' && second == ']') || (first == '{' && second == '}');
+    }
+  }
+
+
+}
